8983516e8d8b3bff54ae10278169ae2c7e5d245f
Usama17git <usamaali78692110@gmail.com>
2026-05-20 15:13:51 +0500

    Initial commit

---
+++ tests/locators.specs.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login Page', () => {
+
+test('Should allow user to log in with valid credentials', async ({ page }) => {
+
+  // Navigate to website
+
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+    // Verify page title
+    await page.getByRole('textbox', { name: 'Username' }).fill('student');
+    await page.getByRole('textbox', { name: 'Password' }).fill('Password123'); 
+
+await page.getByRole('button', { name: 'Submit' }).click();
+
+});
+});
